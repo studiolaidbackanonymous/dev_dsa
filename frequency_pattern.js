dd569fe2fa2c7e0b4b2dca7817f7ee2b798acfb3
anirudh ramaswamy <laidbackanonymous@gmail.com>
2022-10-14 15:19:25 +0530

    practise

--- frequency_pattern.js
+++ frequency_pattern.js
@@ -11,8 +11,8 @@
 //return false if length of arr1 and arr2 are not same
 
 //sqaure  each item in arr1 and compare each item with corresponding item in arr2
-//break at the point when the camparsion is false and return false as output of the function
-// return true if all comparison passes.
+
+// return true if two arrays are same, if not return false.
 
 const same = (arr1, arr2) => {
   if (arr1.length != arr2.length) return false;
